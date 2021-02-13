4681ed132ca2d38c628d41d982a54a80002f6261
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-13 17:30:42 +0000

    adding post request router

--- server/view/router.js
+++ server/view/router.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import controller from '../controller/user.js'
 
 
 const router = express.Router()
 
-router.Router('/')
+router.route('/register')
+  .post(controller.addUser)
 
+router.route('/songs')
+  .get(controller.getAllSongs)
+
+router.route('/song:id')
+  .get(controller.getOneSong)
+  .put(controller.replaceOneSong)
+  .patch(controller.editOneSong)
+  .delete(controller.deleteOneSong)
+
+export default router
