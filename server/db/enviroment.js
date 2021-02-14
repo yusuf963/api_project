44d7553bfe74d5e8fdc40f3d40d905a905ce0ff0
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-14 21:22:36 +0000

    addding const to port

--- server/db/enviroment.js
+++ server/db/enviroment.js
@@ -1,3 +1,3 @@
-export const dbURI = 'mongodb://localhost/songbd'
+export const dbURI = 'mongodb://localhost/songdb'
 export const port = 5000
 export const secret = 'This is a really long secret string that only we know sldanflsdf;dlskanflksdajfl;k'
