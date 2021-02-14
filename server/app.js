09f81aab4ccbab2e9359abe516b87a3413b83a0d
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-14 21:13:37 +0000

    adding user log in router

--- server/app.js
+++ server/app.js
@@ -2,17 +2,19 @@ import express from 'express'
 import router from './view/router.js'
 import bodyParser from 'body-parser'
 import connectToDb from './db/connectToDd.js'
+import errorHandler from './middleware/errorHandling.js'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const startServer = async () => {
-  connectToDb()
+  await connectToDb()
+  console.log('connecting to db')
   app.use(express.json())
 
   app.use('/api', router)
-
-
+  // error handeler middle ware
+  app.use(errorHandler)
 
   //choosing either defult enviroment port or a 5000 port to run server
   const port = process.env.PORT || 5000
