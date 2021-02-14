0a275169441af7d1407c04bfe68c0afb09dcb52b
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-14 21:14:44 +0000

    building logic to handle user log in with password validation

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,13 +1,15 @@
 import UserData from '../model/userSchema.js'
 import songData from '../model/songSchema.js'
+import jwToken from 'jsonwebtoken'
+import { secret } from '../db/enviroment.js'
 
 
-//user registeration
-const addUser = async (req, res, next) => {
+//user rigesteration
+const userRigerter = async (req, res, next) => {
   const user = req.body
   try {
     const newUser = await UserData.create(user)
-    res.status(201).send(`hi ${newUser.name} you successfully registerd`)
+    res.status(201).send(`hi ${newUser.userName}, you successfully rigestered`)
   } catch (err) {
     res.send('something went wrong, please try again')
     console.log(err)
@@ -15,11 +17,33 @@ const addUser = async (req, res, next) => {
   }
 }
 
+// User login
+const userLogIn = async (req, res, next) => {
+  const login = req.body
+  const password = req.body.password
+  try {
+    const checkingUserExist = await UserData.findOne({ email: req.body.email })
+    if (!checkingUserExist.validatePassword(password)) {
+      return res.status(401).res.send('Unothrized')
+    }
+    const token = jwToken.sign(
+      { userId: checkingUserExist._id },
+      secret,
+      { expiresIn: '24h' }
+    )
+    res.send(`welcome ${login.userName}... here is your Token ${token}`)
 
+  } catch (err) {
+    res.send('something wen wrong with your login')
+    next(err)
+  }
+
+}
 //geting all songs
 const getAllSongs = async (req, res, next) => {
+  const songList = await songData.find()
   try {
-    res.send(songData)
+    res.send(songList)
   } catch (err) {
     res.send('something went wrong')
     next()
@@ -29,8 +53,56 @@ const getAllSongs = async (req, res, next) => {
 const getOneSong = async (req, res, next) => {
   const id = req.params.id
   try {
-    const oneSong = songData.findById(id)
-    res.send(oneSong)
+    const oneSong = await songData.findById(id)
+    res.send(` there is the song you aske for: ${oneSong}`)
+  } catch (err) {
+    res.send('something went wrong')
+    next()
+  }
+}
+
+//post song
+const postSong = async (req, res, next) => {
+  const body = await req.body
+  try {
+    const newSong = await songData.create(body)
+    res.send('you successfully created a new song')
+  } catch (err) {
+    res.send('something went wrong')
+    next()
+  }
+}
+//replace one song
+const replaceOneSong = async (req, res, next) => {
+  const id = req.params.id
+  const body = req.body
+  try {
+    const replacedSong = await songData.findByIdAndReplace(id, body)
+    res.send(`you have replaced all the details of ${req.body.songName} song into this ${replacedSong}`)
+  } catch (err) {
+    res.send('something went wrong with replaceing all details by new details')
+    next()
+  }
+}
+//delete one song
+const deleteOneSong = async (req, res, next) => {
+  const id = req.params.id
+  try {
+    const deletedSong = await songData.findByIdAndRemove(id)
+    res.send(` you are successfully deleted this song: ${deletedSong}`)
+  } catch (err) {
+    console.log(err)
+    res.send('something went wrong')
+    next()
+  }
+}
+//edit one song
+const editOneSong = async (req, res, next) => {
+  const id = req.params.id
+  const body = req.body
+  try {
+    const editedSong = await songData.findByIdAndUpdate(id, body, { new: true })
+    res.send('you successfully edited this song')
   } catch (err) {
     res.send('something went wrong')
     next()
@@ -38,10 +110,16 @@ const getOneSong = async (req, res, next) => {
 }
 
 export default {
-  addUser,
+  userRigerter,
+  userLogIn,
   getAllSongs,
+  postSong,
   getOneSong,
   replaceOneSong,
   editOneSong,
   deleteOneSong
 }
+
+// 	"email": "Tara@gmail.com",
+// 	"password": "123"
+// }
