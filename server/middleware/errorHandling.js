5370823c5d556df688fa81b8f8e8b65a7f408c29
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-14 21:22:03 +0000

    creating error handler func in middleware file

---
+++ server/middleware/errorHandling.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    res.status(422).send(err)
+  } else if (err.name === 'CastError') {
+    res.status(404).send(err)
+  } else {
+    res.status(500).send(err)
+  }
+  next()
+}
+
+export default errorHandler
