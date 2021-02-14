07aaa5a3d0585b5f93c2a6544383bac7303934fa
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-14 21:16:09 +0000

    updating user schema

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,15 +1,33 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 import mongooseHidden from 'mongoose-hidden'
-import mongooseUniqueValidator from 'mongoose-unique-validator'
+import uniqueValidator from 'mongoose-unique-validator'
 
 // song Schema
 const schema = new mongoose.Schema({
   userName: { type: String, require: true, unique: true },
   email: { type: String, require: true },
   password: { type: String, require: true }
-},
-  { timestamp: true }
-)
+})
 
-export default mongoose.model('User', schema)
+schema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync())
+  next()
+})
+
+schema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compare(password, this.password)
+}
+
+schema.plugin(uniqueValidator)
+schema.plugin(mongooseHidden({ defaultHidden: { password: true, email: true } }))
+
+const User = mongoose.model('User', schema)
+
+const user = new User({
+  userName: 'Henry',
+  email: 'henry@gmail.com',
+  password: 'hhjhjj3643'
+})
+// user.save()
+export default User
