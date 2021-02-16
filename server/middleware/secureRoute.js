013da9f5d03e662a11df8128d5f1bfed2261f17b
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-16 15:49:26 +0000

    building up logic for secureroute level security

---
+++ server/middleware/secureRoute.js
@@ -0,0 +1,31 @@
+import jwToken from 'jsonwebtoken'
+import User from '../model/userSchema.js'
+import { secret } from '../db/enviroment.js'
+
+const secureRoute = async (req, res, next) => {
+  const authToken = req.headers.authorization
+  try {
+    if (!authToken || !authToken.startsWith('Bearer')) {
+      return res.status(401).send('Unauthorized')
+    }
+    const token = authToken.replace('Bearer ', '')
+    jwToken.verify(token, secret, async (err, data) => {
+      if (err) {
+        return res.status(401).send('Unauthorized')
+      }
+      const user = await User.findById(data.useId)
+      if (!user) {
+        return res.status(401).send('Unauthorized')
+      }
+      //this for object level permission
+      req.currentUser = user
+      next()
+    })
+
+
+  } catch (err) {
+    res.status(401).send({ message: 'Unauthorized' })
+  }
+
+}
+export default secureRoute
