2c9cb3901751e7bb8e6a580eaaf0bef2b87ed9c1
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-16 15:52:37 +0000

    importing logger and errro handler and inplament them in

--- server/app.js
+++ server/app.js
@@ -3,21 +3,24 @@ import router from './view/router.js'
 import bodyParser from 'body-parser'
 import connectToDb from './db/connectToDd.js'
 import errorHandler from './middleware/errorHandling.js'
+import logger from './middleware/logger.js'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const startServer = async () => {
+  //connecting via mangoose to DB
   await connectToDb()
   console.log('connecting to db')
   app.use(express.json())
-
+  //logging request
+  app.use(logger)
+  //pre rute key
   app.use('/api', router)
   // error handeler middle ware
   app.use(errorHandler)
-
   //choosing either defult enviroment port or a 5000 port to run server
-  const port = process.env.PORT || 5000
+  const port = process.env.PORT || 8000
   app.listen(port, () => console.log(`server up and running on port${port}`))
 }
 
