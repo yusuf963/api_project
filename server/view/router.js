43aaf0604dcbc932ded5e898008fb66eb8a4dbec
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-16 15:47:27 +0000

    creating

--- server/view/router.js
+++ server/view/router.js
@@ -1,26 +1,28 @@
 import express from 'express'
 import controller from '../controller/user.js'
+import secureRoute from '../middleware/secureRoute.js'
 
 
 const router = express.Router()
 // user rigesteration
-router.route('/rigester')
-  .post(controller.userRigerter)
-
+router.route('/register')
+  .post(controller.userRegirter)
 // user LOGIN
 router.route('/login')
   .post(controller.userLogIn)
 
+
 //all songs
 router.route('/songs')
   .get(controller.getAllSongs)
-  .post(controller.postSong)
+  //secureroute for route level permission
+  .post(secureRoute, controller.postSong)
 
 //one song
 router.route('/song/:id')
   .get(controller.getOneSong)
-  .put(controller.replaceOneSong)
-  .patch(controller.editOneSong)
-  .delete(controller.deleteOneSong)
+  .put(secureRoute, controller.replaceOneSong)
+  .patch(secureRoute, controller.editOneSong)
+  .delete(secureRoute, controller.deleteOneSong)
 
 export default router
