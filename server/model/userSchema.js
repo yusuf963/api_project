2684827d77562b6261c9ab2d6a6704f08c19afde
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-18 00:41:48 +0000

    adding comment schema

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -4,24 +4,43 @@ import mongooseHidden from 'mongoose-hidden'
 import uniqueValidator from 'mongoose-unique-validator'
 
 // song Schema
-const schema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   userName: { type: String, require: true, unique: true },
-  email: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
   password: { type: String, require: true }
+  // isAdmin: { type: Boolean }
 })
 
-schema.pre('save', function (next) {
+userSchema.pre('save', function (next) {
   this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync())
   next()
 })
 
-schema.methods.validatePassword = function validatePassword(password) {
+userSchema.methods.validatePassword = function validatePassword(password) {
   return bcrypt.compare(password, this.password)
 }
 
-schema.plugin(uniqueValidator)
-schema.plugin(mongooseHidden({ defaultHidden: { password: true, email: true } }))
+// password Confiration
 
-const User = mongoose.model('User', schema)
+// userSchema.virtual('pawwordConfirmation').set(function setPasswordConfirmation(passwordConfirmation) {
+//   this._passwordConfirmation = passwordConfirmation
+// })
+
+// userSchema.pre('validate', function checkPassword(next) {
+//   if (this.isModified('password') && (this.password !== this._passwordConfirmation)) {
+//     this.invalidate('passwordConfirmation', 'should match password')
+//   }
+//   next()
+// })
+
+
+//password confirmaton ends here
+
+
+
+userSchema.plugin(uniqueValidator)
+userSchema.plugin(mongooseHidden({ defaultHidden: { password: true, email: true } }))
+
+const User = mongoose.model('User', userSchema)
 
 export default User
