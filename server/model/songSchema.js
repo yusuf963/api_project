6dea8ee142ccc30600477f4b7c950e76a50c985b
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-18 00:43:40 +0000

    adding user refrence to songschema

--- server/model/songSchema.js
+++ server/model/songSchema.js
@@ -1,13 +1,26 @@
 import mongoose from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
+
+const commentSchema = new mongoose.Schema({
+  text: { type: String, require: true },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', require: true }
+
+}, {
+  timestamps: true
+})
 
 const songSchema = new mongoose.Schema({
-  title: { type: String, require: true },
+  title: { type: String, require: true, unique: true },
   src: { type: String, require: true },
   rate: { type: Number, min: 1, max: 9 },
-  reviews: { type: String, timeStamp: true },
-  user: { type: mongoose.Schema.ObjectId, ref: 'Users', require: true }
+  reviews: { type: String },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', require: true },
+  comments: [commentSchema]
+}, {
+  timeStamp: true
 })
 const Song = mongoose.model('Song', songSchema)
 
+commentSchema.plugin(uniqueValidator)
+export default Song
 
-export default Song 
